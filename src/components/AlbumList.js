3feb4520707dae51e6915e8323ff7e4ec24bcf14
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2017-06-14 11:17:57 -0400

    fetched data and displayed it inside a list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,18 +2,27 @@
 
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] };
+
     componentWillMount() {
-        
+        const url = 'https://rallycoding.herokuapp.com/api/music_albums';
+        axios.get(url)
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
     }
 
     render() {
         return (
             <View>
-                <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
-        )
+        );
     }
 }
 
